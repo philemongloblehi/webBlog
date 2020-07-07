8e1a36923ebb126c0ca42487055d71fee81ff996
Philemon GLOBLEHI <philemongloblehi@gmail.com>
2020-07-07 20:05:45 +0000

    Implementation CRUD article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -9,6 +9,26 @@ class BlogController extends AbstractController
 {
     public function index()
     {
-        return $this->render('blog/index.html.twig');
+        return new Response('<h1>Page d\'accueil</h1>');
+    }
+
+    public function add()
+    {
+        return new Response('<h1>Ajouter un article</h1>');
+    }
+
+    public function show($url)
+    {
+        return new Response('<h1>Lire un article ' . $url . '</h1>');
+    }
+
+    public function edit($id)
+    {
+        return new Response('<h1>Modifier l\'article '. $id . '</h1>');
+    }
+
+    public function remove($id)
+    {
+        return new Response('<h1>Supprimer l\'article '. $id .'</h1>');
     }
 }
