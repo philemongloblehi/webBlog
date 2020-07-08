ee416dec7b4dc7a544d4e3b690ceb84f30de0e03
Philemon GLOBLEHI <philemongloblehi@gmail.com>
2020-07-08 06:54:14 +0000

    Implementation des vues twig

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,36 +5,114 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Form\ArticleType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends AbstractController
 {
     public function index()
     {
-        return $this->render('blog/index.html.twig');
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findBy(
+        ['isPublished' => true],
+        ['publicationDate' => 'desc']
+        );
+
+        return $this->render('blog/index.html.twig', [
+           'articles' => $articles
+        ]);
     }
 
-    public function add()
+    public function add(Request $request)
     {
         $article = new Article();
         $form = $this->createForm(ArticleType::class, $article);
 
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setLastUpdateDate(new \DateTime());
+
+            if ($article->getPicture() !== null) {
+                $file = $form->get('picture')->getData();
+                $fileName =  uniqid(). '.' .$file->guessExtension();
+
+                try {
+                    $file->move(
+                        $this->getParameter('images_directory'), // Le dossier dans le quel le fichier va etre charger
+                        $fileName
+                    );
+                } catch (FileException $e) {
+                    return new Response($e->getMessage());
+                }
+
+                $article->setPicture($fileName);
+            }
+
+            if ($article->getIsPublished()) {
+                $article->setPublicationDate(new \DateTime());
+            }
+
+            $em = $this->getDoctrine()->getManager(); // On récupère l'entity manager
+            $em->persist($article); // On confie notre entité à l'entity manager (on persist l'entité)
+            $em->flush(); // On execute la requete
+
+            return new Response('L\'article a bien été enregistrer.');
+        }
+
         return $this->render('blog/add.html.twig', [
-           'form' => $form->createView()
+            'form' => $form->createView()
         ]);
     }
 
-    public function show($url)
+    public function show(Article $article)
     {
         return $this->render('blog/show.html.twig', [
-            'slug' => $url
+            'article' => $article
         ]);
     }
 
-    public function edit($id)
+    public function edit(Article $article, Request $request)
     {
+        $oldPicture = $article->getPicture();
+
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setLastUpdateDate(new \DateTime());
+
+            if ($article->getIsPublished()) {
+                $article->setPublicationDate(new \DateTime());
+            }
+
+            if ($article->getPicture() !== null && $article->getPicture() !== $oldPicture) {
+                $file = $form->get('picture')->getData();
+                $fileName = uniqid(). '.' .$file->guessExtension();
+
+                try {
+                    $file->move(
+                        $this->getParameter('images_directory'),
+                        $fileName
+                    );
+                } catch (FileException $e) {
+                    return new Response($e->getMessage());
+                }
+
+                $article->setPicture($fileName);
+            } else {
+                $article->setPicture($oldPicture);
+            }
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return new Response('L\'article a bien été modifier.');
+        }
+
         return $this->render('blog/edit.html.twig', [
-            'slug' => $id
+            'article' => $article,
+            'form' => $form->createView()
         ]);
     }
 
